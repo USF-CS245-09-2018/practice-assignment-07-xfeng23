5d62282f9761adb43c5ac42895c1763eef4a237d
XueFeng <xfeng23@usfca.edu>
2018-10-19 13:42:09 -0700

    update.

---
+++ ArrayList.java
@@ -0,0 +1,34 @@
+
+public class ArrayList implements List{
+
+	@Override
+	public void add(Object obj) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void add(int pos, Object obj) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Object get(int pos) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Object remove(int pos) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int size() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
